e87103ca38be5dfb50229884adc455ba16ff4835
Blake Collins <blakeac369@gmail.com>
2020-01-18 14:24:04 -0800

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,40 +1,16 @@
-function startup() {
-    var $imgs = $(".thumbnail a");
-    var $search = $("#search-filter");
-    var $noResults = $("#no-results")[0];
-    var $topButton = $(".nav .top")[0];
+$(".photoGallery").hide().delay(500).fadeIn(1000);
 
-   
+const $gallery_items = $(".photoGallery a");
 
-    // Declaring a function which will be called when the search input
-    // keyup event is trigged (via user text input into the field)
-    const filterImages = function() {
-        // 'this' is $search element
-        const searchValue = this.value.trim().toLowerCase();
-        var resultsFound = false;
-
-        $imgs.each(function(index, img) {
-            const title = img.title.trim().toLowerCase();
-            
-            if (title.includes(searchValue)) {
-                img.style.display = '';
-                resultsFound = true;
-            } else {
-                img.style.display = 'none';
-            }
-        });
-
-        if (resultsFound) {
-            $noResults.style.display = "none";
-            $topButton.style.display = "";
-        } else {
-            $noResults.style.display = "";
-            $topButton.style.display = "none";
-        }
+$("#imageSearch").on("keyup", function(event) {
+  let $search = $(event.target).val().toUpperCase();
+  for ( let i = 0 ; i < $gallery_items.length ; i += 1) {
+    let $imageSearch = $gallery_items.eq(i);
+    if ($imageSearch.attr("data-title").toUpperCase().indexOf($search) === -1) {
+      $imageSearch.fadeOut(500);
+    } 
+    if ($imageSearch.attr("data-title").toUpperCase().indexOf($search) !== -1) {
+      $imageSearch.hide().fadeIn(500);
     }
-
-    // Calls filterImages on every keyup
-    $search.on('keyup', filterImages);
-}
-
-startup();
+  }
+});
